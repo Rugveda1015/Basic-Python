e80e04190afcc8a7abff666fe1e741c4072a2557
Rugveda Gaonkar <135946030+Rugveda1015@users.noreply.github.com>
2023-06-13 10:26:01 +0530

    Add files via upload

---
+++ simple intrest.py
@@ -0,0 +1,20 @@
+def Interest():   
+P = float(input("Enter your principal Amount\n"))
+
+R = float(input("Enter your rate interest on the amount\n"))
+
+T = float(input("Enetr the duarationfor amount to be desposited in the bank\n"))
+
+
+I  = P*R*T/100
+
+print("Your Interset on the amount is given by\n", I)
+
+A = P+I
+
+print("Your total amount on the principal amount  is give by\n", A)
+
+
+Interest()
+    
+
