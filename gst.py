e81d75b1d70374769019653590d828cfc383e3f8
Rugveda Gaonkar <135946030+Rugveda1015@users.noreply.github.com>
2023-06-13 10:26:22 +0530

    Add files via upload

---
+++ gst.py
@@ -0,0 +1,5 @@
+O= float(input("Enter the orignal value for calculatinng the gst\n"))
+G= float(input("Enter the value for calculating the value of gst\n"))
+
+GST= (O*G)/100
+print("print the value for the gst rate of the user",GST)
