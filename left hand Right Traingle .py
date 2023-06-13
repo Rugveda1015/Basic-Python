07f94272836086aa616e53e1af3f71d40f713d40
Rugveda Gaonkar <135946030+Rugveda1015@users.noreply.github.com>
2023-06-13 10:21:46 +0530

    Add files via upload

---
+++ left hand Right Traingle .py
@@ -0,0 +1,9 @@
+for i in range(65,70):
+# i is starting 64 to 69
+    for j in range(65,i+1):
+# j is 65,66 = 65
+# j is 65,67 = 65,66
+# j is 65,68 = 65,66,67
+        print(chr(i),end = " ")
+ #print(chr(j), end=" ")
+    print("\n")
